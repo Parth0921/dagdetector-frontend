fda9e0bb7423fc95c32cb2eb95ed7b98dae839fd
Parth Rathod <parth.rathod12@gmail.com>
2024-10-26 20:07:06 +0530

    Task 4 complete with backend need to display the user in a graceful manner

--- src/submit.js
+++ src/submit.js
@@ -1,10 +1,43 @@
 // submit.js
 
+import { useStore } from "./store";
+
 export const SubmitButton = () => {
+    
+    const { nodes, edges } = useStore();
+    const handleClick = async (e) => {
+        e.preventDefault();
+        const nodesList = nodes.map((node) => node.id);
+        const edgesList = edges.map((edge) => {
+            return {
+                source: edge.source,
+                target: edge.target
+            }
+        })
+
+        const payload = {
+            nodes: nodesList,
+            edges: edgesList
+        }
+        
+        try {
+            const response = await fetch('http://localhost:8000/pipelines/parse', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(payload), 
+            })
+            const data = await response.json();
+            console.log(data);
+        } catch (e) {
+            console.log(e);
+        }
+    }
 
     return (
         <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
-            <button type="submit">Submit</button>
+            <button type="submit" onClick={handleClick}>Submit</button>
         </div>
     );
 }
